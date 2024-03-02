ce74de722d135406b591f9808289873526651671
Aria Argenta Silva Castañeda <55609849+ariargenta@users.noreply.github.com>
2024-03-01 21:31:28 -0600

    initialize repository

---
+++ frontend/src/main.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  createTheme,
+  ThemeProvider
+} from '@mui/material'
+
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Lato, serif",
+    allVariants: { color: "white"},
+  }
+})
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
